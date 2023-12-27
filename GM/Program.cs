53d725894b1f2806dda2731c145501359153fd82
MouadNadzi <mouadnadzi10@example.com>
2023-12-27 15:08:57 +0100

    tes

--- GM/Program.cs
+++ GM/Program.cs
@@ -12,11 +12,8 @@ namespace GM
             // see https://aka.ms/applicationconfiguration.
             // console.writeline hello mouad;
 
-<<<<<<< HEAD
-            // hello from ilyess
-=======
             // hello from ilyiss
->>>>>>> 7e5c6ec48127b989afe7b1fda5361365df9592bf
+            // hello from ilyess
 
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             ApplicationConfiguration.Initialize();
