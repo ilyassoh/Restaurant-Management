b2e60de013545441fd041ee60515480fcc327f05
L-Yes <ilyass.ham16@gmail.com>
2023-12-27 14:59:29 +0100

    new line

--- GM/Program.cs
+++ GM/Program.cs
@@ -11,6 +11,9 @@ namespace GM
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             // console.writeline hello mouad;
+
+            // hello from ilyass
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             ApplicationConfiguration.Initialize();
             Application.Run(new Login_Form());
