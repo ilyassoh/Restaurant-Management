345fec9e6cef35fac0411bf526eae4254fa296c5
MouadNadzi <mouadnadzi10@example.com>
2023-12-31 18:38:01 +0100

    form login first test

--- GM/test.cs
+++ GM/test.cs
@@ -5,16 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using Microsoft.VisualBasic.ApplicationServices;
 
 namespace GM
 {
-    internal class test
+    class test
     {
         //hello form ilyass
         // hello from oumaima
        //public static readonly con_string = "server=localhost;database=gestion_magasin;uid=root;pwd=;";
-        public static readonly con_string = "Data Source=MouadPC; Initial Catalog=GM;Persist Security Info-True; User ID=e; Password=123;";
-        public static SqlConnectin con = new SqlConnection(con_string);
+        public static string con_string = "Data Source=MouadPC; Initial Catalog=GM;Persist Security Info-True; User ID=e; Password=123;";
+        public static SqlConnection con = new SqlConnection(con_string);
 
         public static bool IsValide(string user, string pass)
         {
@@ -27,9 +28,24 @@ namespace GM
             da.Fill(dt);
             if (dt.Rows.Count > 0)
                 isValide = true;
+            con.Close();
             return isValide;
-            
         }
+
+        /*con.Open();
+string query = "select * from login where username='" + user + "' and password='" + pass + "'";
+        SqlCommand cmd = new SqlCommand(query, con);
+        SqlDataReader dr = cmd.ExecuteReader();
+if (dr.Read())
+{
+    con.Close();
+    return true;
+}
+else
+{
+    con.Close();
+    return false;
+}*/
 
     }
 }
