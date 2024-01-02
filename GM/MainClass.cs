b12b5214ecd70bf1a323b1a7368884803f48c039
L-Yes <ilyass.ham16@gmail.com>
2024-01-02 11:29:28 +0100

    main class

--- GM/MainClass.cs
+++ GM/MainClass.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using System.Linq;
@@ -12,5 +14,25 @@ namespace GM
     {
         public static readonly string con_string = " Data Source=L-yes\\SQLEXPRESS;Initial Catalog=GM;User ID=sa;Password=123;";
         public static SqlConnection con = new SqlConnection(con_string) ;
+
+        public static bool IsValidUser(string user, string pass)
+        {
+            bool IsValid = false;
+
+            string query = @"Select * from users username ='" + user + "' and upass ='" + pass + "'";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                IsValid = true;
+            }
+       
+                con.Close();
+
+            return IsValid; 
+        }
     }
 }
